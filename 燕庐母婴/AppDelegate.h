c95d33b2014836e27b71df9fefe58ef550b45b03
董飞剑 <dongfeijian@Dong.local>
2015-05-28 20:38:29 +0800

    Initial Commit

---
+++ 燕庐母婴/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  燕庐母婴
+//
+//  Created by 董飞剑 on 15/5/28.
+//  Copyright (c) 2015年 燕庐科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
